d79c57d6f29fc90e13945321a767d0622a9d75e2
Farid Ahmed <130706495+FaridCS@users.noreply.github.com>
2024-10-03 20:09:52 +0600

    added a template

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include "bits/stdc++.h"
+using namespace std;
+const int MOD = 1e9 + 7;
+const double PI = acos(-1);
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define FAST                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0)
+void uttor()
+{
+}
+int main()
+{
+    FAST;
+    int T;
+    cin >> T;
+    while (T--)
+        uttor();
+    return 0;
+}
